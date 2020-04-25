0a72fc2b274fe381ae96b1033afe0883687eba32
Datarius <MarcelRoth100@aol.com>
2020-04-25 13:16:46 +0200

    GameLoop

--- src/sample/GameLoop.java
+++ src/sample/GameLoop.java
@@ -6,7 +6,6 @@ import java.util.Random;
 
 public class GameLoop extends Thread implements Runnable {
 
-
     private Canvas canvas;
     private GraphicsContext gc;
 
@@ -18,58 +17,13 @@ public class GameLoop extends Thread implements Runnable {
 
     public void run() {
 
-        Random rnd = new Random();
-        double ups = 60;
-        double fps = 60;
 
-        long initialTime = System.nanoTime();
-        final double timeU = 1000000000 / ups;
-        final double timeF = 1000000000 / fps;
-        double deltaU = 0, deltaF = 0;
-        int frames = 0, ticks = 0;
-        long timer = System.currentTimeMillis();
-
-        while (true) {
-
-            long currentTime = System.nanoTime();
-            deltaU += (currentTime - initialTime) / timeU;
-            deltaF += (currentTime - initialTime) / timeF;
-            initialTime = currentTime;
-
-            if (deltaU >= 1) {
-
-                //getInput();
-                update();
-                ticks++;
-                deltaU--;
-            }
-
-
-            if (deltaF >= 1) {
-                long b = System.currentTimeMillis();
-                clearScreen();
-
-
-                draw();
-                long a = System.currentTimeMillis();
-
-                frames++;
-                deltaF--;
-            }
-
-            if (System.currentTimeMillis() - timer > 1000) {
-                frames = 0;
-                ticks = 0;
-                timer += 1000;
-            }
-        }
     }
 
 
     private void initialize()
     {
         gc = canvas.getGraphicsContext2D();
-
     }
 
     private void update(){
